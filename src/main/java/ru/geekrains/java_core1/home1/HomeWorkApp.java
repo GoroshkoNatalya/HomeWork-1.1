89e4c85ff03fa665a0389e2fa2c05eb4b9905a84
GoroshkoNatalya <goroshko_natalya@inox.ru>
2024-07-27 13:39:58 +0300

    .

--- src/main/java/ru/geekrains/java_core1/home1/HomeWorkApp.java
+++ src/main/java/ru/geekrains/java_core1/home1/HomeWorkApp.java
@@ -9,7 +9,6 @@ public class HomeWorkApp {
         compareNumbers();
 
     }
-//
     public static void printThreeWorlds(){
         System.out.println("Orange");
         System.out.println("Banana");
